3871449c8acee9c116d49fa1a43b6933070861f0
shinae <shinae10230310@gmail.com>
2025-04-04 23:05:42 +0900

    controller작성

--- src/main/java/mutsaweek2/demo/controller/ArticleController.java
+++ src/main/java/mutsaweek2/demo/controller/ArticleController.java
@@ -1,16 +1,38 @@
 package mutsaweek2.demo.controller;
 
+import lombok.RequiredArgsConstructor;
 import mutsaweek2.demo.Article;
 import mutsaweek2.demo.service.ArticleService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController//restapi의 진입점
+@RequiredArgsConstructor
 public class ArticleController {
-    private ArticleService articleService;
 
-    @PostMapping("/articles/write")
-    public String write(Article article) {
+    private final ArticleService articleService; //서비스 계층 의존성주입
 
+    @PostMapping("/api/v1/articles")
+    public ResponseEntity<Long> writeArticle(@RequestBody Article article) {
+        return ResponseEntity.status(201).body(articleService.writeArticle(article));
+    } // 게시글 작성에 필요한
+
+    @GetMapping("/api/v1/article/{articleId}")
+    public ResponseEntity<Article> getArticle(@PathVariable Long articleId) {
+        return ResponseEntity.ok(articleService.findById(articleId));
+    }
+
+    @GetMapping("/api/v1/articles")
+    public ResponseEntity<List<Article>> getAllArticles() {
+        return ResponseEntity.ok(articleService.findAll());
+    }
+
+    @GetMapping("/api/v1/articles/search")
+    public ResponseEntity<List<Article>> searchArticles(@RequestParam String title) {
+        return ResponseEntity.ok(articleService.findByTitle(title));
     }
 }
+
